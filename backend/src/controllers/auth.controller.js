73a13d3a90a339d9fda30b865c2d480cd1d67870
irohanrajput <itusharsingh27@gmail.com>
2025-02-24 18:14:26 +0530

    done with registration, login, logout

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,10 +1,13 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-import {generateToken} from "../lib/utils.js";
+import { generateToken } from "../lib/utils.js";
 
 export const registerUser = async (req, res) => {
   const { fullName, password, email } = req.body;
   try {
+    if (!fullName || !password || !email) {
+      return res.status(400).json({ message: "Please fill in all fields" });
+    }
     if (password.length < 6) {
       return res
         .status(400)
@@ -34,19 +37,53 @@ export const registerUser = async (req, res) => {
       _id: user._id,
       fullName: user.fullName,
       email: user.email,
-      token
+      token,
     });
-
-
   } catch (error) {
-    return res.status(500).json({ message: error.message });
+    console.log("Signup Controller Error: ", error.message);
+    return res.status(500).json({ message: "Something went wrong" });
   }
 };
-export const loginUser = (req, res) => {
-  // Implement login logic here
-  res.send("login route");
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      return res.status(400).json({ message: "Please fill in all fields" });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "Invalild credentials or User not found" });
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res
+        .status(400)
+        .json({ message: "Invalid credentials or User not found" });
+    }
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      message: "User logged is Successfully",
+      user: {
+        id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+        displayPicture: user.displayPicture,
+      },
+    });
+  } catch (error) {
+    console.log("error in the loginUser Controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 export const logoutUser = (req, res) => {
-  // Implement logout logic here
-  res.send("logout route");
+  try {
+    res.clearCookie("token");
+    res.status(200).json({ message: "User logged out successfully" });
+  } catch (error) {
+    console.log("error in the logoutUser Controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
